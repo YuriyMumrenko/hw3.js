5f7559cc0b943bac6576180373d4c44019407cec
Yuriy <mumrenkoyuriy@gmail.com>
2017-07-19 17:42:12 +0300

    array output is modified

--- js/3.js
+++ js/3.js
@@ -13,7 +13,7 @@ let buttonPush = document.querySelector('.push');
 
 //-----------слушатели----------------------
 
-buttonEnterArr.addEventListener('click', arrOut);
+ buttonEnterArr.addEventListener('click', arrInput);
 buttonPop.addEventListener('click', popRemoveLastElement);
 buttonShift.addEventListener('click', shiftRemoveFirsElement);
 buttonPush.addEventListener('click', pushAddEnd);
@@ -23,37 +23,44 @@ buttonPush.addEventListener('click', pushAddEnd);
 let arr1 = [];
 let p = document.getElementById('new');
 
-//------------------------------------------
-function add (arr){
-	p.textContent = arr;
+//--------ф-я вывода массива (индекс -> значение)---
+
+function arrOut(arr){
+    
+var toStringArr = (arr1) => arr1.map( (inputArrayValue, inputArrayIndex) => {
+let str = '';
+str += `array[${inputArrayIndex}] = ${inputArrayValue}`;
+return str;
+});
+ p.textContent = toStringArr(arr);
 }
 
-//-----------ф-я вывода--------------------
-function arrOut(){
+//--------ф-я ввода в массив индекса и значения--------
 
- arr1 [inputArrayIndex.value] = inputArrayValue.value;
- add (arr1);
+function arrInput(){
+	arr1 [inputArrayIndex.value] = inputArrayValue.value;
+	arrOut(arr1);
 }
 
 //------ф-я добавляет значение в конец массива--------
 
 function pushAddEnd(){
 	arr1.push(inputArrayValue.value);
-	add (arr1);
+	arrOut (arr1);
 }
 
 //------ф-я удаляет значение в конце массива--------
 
 function popRemoveLastElement(){
 	arr1.pop();
-	add (arr1);
+	arrOut (arr1);
 }
 
 //------ф-я удаляет значение в начале массива--------
 
 function shiftRemoveFirsElement(){
 	arr1.shift();
-	add (arr1);
+	arrOut (arr1);
 }
 
 //------------------------------------------------
